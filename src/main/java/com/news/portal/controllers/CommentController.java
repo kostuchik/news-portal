cfff548234f34937f62ecb6a79c82c47ada69a35
kostuchik <it.kostuchik@gmail.com>
2023-12-23 18:42:20 +0300

    [COMMENTS] BE: added methods to CommentController

--- src/main/java/com/news/portal/controllers/CommentController.java
+++ src/main/java/com/news/portal/controllers/CommentController.java
@@ -1,9 +1,39 @@
 package com.news.portal.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.news.portal.models.Comment;
+import com.news.portal.services.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/{commentId}")
+    public ResponseEntity<Comment> getComment(@PathVariable Long commentId) {
+        return ResponseEntity.ok().body(commentService.findCommentById(commentId));
+    }
+
+    @PostMapping("/{newsId}")
+    public ResponseEntity<?> createComment(@PathVariable Long newsId,
+                                           @RequestBody Comment comment) {
+        return ResponseEntity.ok().body(commentService.createComment(newsId, comment));
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<?> editComment(@PathVariable Long commentId,
+                                         @RequestBody Comment comment) {
+        return ResponseEntity.ok().body(commentService.updateComment( commentId, comment));
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable Long commentId) {
+        commentService.deleteById(commentId);
+        return ResponseEntity.ok().body("User's has been delete.");
+    }
 }
--- src/main/java/com/news/portal/services/CommentService.java
+++ src/main/java/com/news/portal/services/CommentService.java
@@ -1,7 +1,15 @@
 package com.news.portal.services;
 
+import com.news.portal.repositories.CommentRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+
 }
