129a1247c3f36b6ed89571ae6aca120620d659f9
kostuchik <it.kostuchik@gmail.com>
2024-01-20 13:04:27 +0300

    [NEWS] BE: added explanations for the SQL request in the newsrepository

--- src/main/java/com/news/portal/repositories/NewsRepository.java
+++ src/main/java/com/news/portal/repositories/NewsRepository.java
@@ -28,9 +28,11 @@ public interface NewsRepository extends JpaRepository<News, Long> {
             "(:text is not null and lower(trim(n.description)) like ('%' || lower(trim(:text)) || '%')) or " +
             "(:text is not null and lower(n.text) like ('%' || lower(trim(:text)) || '%')) or " +
             "(:text is null and concat(n.title, n.description, n.text) like ('%'))) and " +
+
             "((n.publicationDate between :fromDate and :toDate) or " +
             "(cast(:fromDate as timestamp ) is null and n.publicationDate <= :toDate) or " +
             "(cast(:toDate as timestamp )  is null and n.publicationDate >= :fromDate)) and " +
+
             "((:toNumberComments is not null and size(n.comments) between :fromNumberComments and :toNumberComments) or " +
             "(:toNumberComments is null and size(n.comments) >= :fromNumberComments))")
     Page<News> searchByKey(@Param("text") String text,
@@ -41,3 +43,19 @@ public interface NewsRepository extends JpaRepository<News, Long> {
                            Pageable pageable);
 
 }
+/*
+    Этот запрос позволяет выполнять динамический поиск новостей с использованием различных параметров
+    ":"  – используется для обозначения параметров
+
+    Условия поиска в тексте:
+    lower(trim(...)) приводит текст к нижнему регистру и удаляет пробелы с начала и конца строки.
+    like ('%' || lower(trim(:text)) || '%') выполняет поиск подстроки текста внутри строки.
+
+    Условие по дате публикации:
+    Проверяется, если дата публикации находится между :fromDate и :toDate.
+    Если :fromDate или :toDate равны null, то используется соответствующее условие.
+
+    Условие по количеству комментариев:
+    Условие выбора новостей в зависимости от количества комментариев.
+    Если :toNumberComments не равен null, используется диапазон, иначе выбираются новости с количеством комментариев больше или равным :fromNumberComments.
+ */
